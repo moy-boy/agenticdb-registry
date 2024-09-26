9242e06336c46293edee98ee706d451e03901ddf
moy-boy <173254869+moy-boy@users.noreply.github.com>
2024-09-25 20:43:59 -0700

    Updating scripts

--- docker_build.sh
+++ docker_build.sh
@@ -1,5 +1,12 @@
+#!/bin/bash
+
 # Uncomment if needed
-# docker rmi agenticdb
+# docker rmi agenticdb:latest
 # docker builder prune --all --force
-docker build --no-cache -t agenticdb .
+
+# Remove all containers based on agenticdb:latest, both stopped and exited
+docker ps -a -q --filter "ancestor=agenticdb:latest" | xargs -r docker rm -f
+
+# Build the new image with the tag agenticdb:latest
+docker build --no-cache -t agenticdb:latest .
 
--- docker_run.sh
+++ docker_run.sh
@@ -1,1 +1,14 @@
+#!/bin/bash
+
+# Uncomment if needed
+# docker rmi agenticdb:latest
+# docker builder prune --all --force
+
+# Remove all containers based on agenticdb:latest, both stopped and running
+docker ps -a -q --filter "ancestor=agenticdb:latest" | xargs -r docker rm -f
+
+# Build the new image with the tag agenticdb:latest
+docker build --no-cache -t agenticdb:latest .
+
+# Run the newly built container with the name agenticdb_container and expose port 8000
 docker run -d --name agenticdb_container -p 8000:8000 agenticdb:latest
